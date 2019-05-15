75c01843e7b7905ec98fc1b5eff5b994218c1dce
fhorvat <fihorvat@yahoo.com>
2019-05-15 16:37:34 +0200

    Added mapping pipeline written in Bash

---
+++ map_pipeline.bash/precise_mapping_scripts/08b.read_hex_URLs.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# ----------------Loading variables------------------- #
+HEX_PATH=%HEX_PATH
+TABLE_PATH=http://hex.bioinfo.hr/~fhorvat/${HEX_PATH#~/public_html}/log.tracks_URL.txt
+
+# ----------------Commands------------------- #
+# get table from hex
+wget $TABLE_PATH
+
+# change permissions
+find . -name "*.bw" -exec chmod 744 {} \;
+find . -name "*.bam*" -not -name "*genome*" -not -name "*rDNA_45S*" -exec chmod 744 {} \;
