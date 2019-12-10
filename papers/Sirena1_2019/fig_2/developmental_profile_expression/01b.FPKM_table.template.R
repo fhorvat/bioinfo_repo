e71182bb76577257938ef5c5f122126f7ea92063
fhorvat <fihorvat@yahoo.com>
2019-12-10 23:56:21 +0100

    Added scripts for Sirena1 paper (review)

--- papers/Sirena1_2019/fig_2/developmental_profile_expression/01b.FPKM_table.template.R
+++ papers/Sirena1_2019/fig_2/developmental_profile_expression/01b.FPKM_table.template.R
@@ -4,10 +4,9 @@
 ### AUTHOR: Filip Horvat
 rm(list = ls()); gc()
 options(bitmapType = "cairo")
-wideScreen()
 
 ######################################################## WORKING DIRECTORY
-setwd("/common/WORK/fhorvat/Projekti/Svoboda/Analyses/developmental_profile_expression/FPKMs")
+setwd("/common/WORK/fhorvat/Projekti/Svoboda/lncRNA_KO/Analysis/lnc1_paper/developmental_profile_expression/FPKMs")
 
 ######################################################## LIBRARIES
 library(dplyr)
@@ -48,22 +47,28 @@ ensembl_version <- 93
 experiment <- "%EXPERIMENT"
 
 # set base experiment path
-base_path <- file.path("/common/WORK/fhorvat/Projekti/Svoboda/Analyses/developmental_profile_expression")
+base_path <- file.path("/common/WORK/fhorvat/Projekti/Svoboda/accessory_data_sets/RNAseq", experiment)
 
 # documentation path
-documentation_path <- file.path(base_path, "Documentation")
+documentation_path <- file.path(base_path, "Data/Documentation")
+
+# mapped path
+mapped_path <- file.path(base_path, "Data/Mapped/STAR_mm10")
 
 # analysis path
-se_main_path <- file.path(base_path, "summarizedExperiments")
+se_main_path <- file.path(inpath, "..", "summarizedExperiments")
 
 # summarizedExperiment path
-se_path <- list.files(path = se_main_path, pattern = str_c("ensembl.", ensembl_version, ".*", experiment, "\\.se\\.RDS"), full.names = T)
+se_path <- list.files(path = se_main_path, pattern = str_c("ensembl.", ensembl_version, ".*", "\\.short_Sirena1\\.", experiment, "\\.se\\.RDS"), full.names = T)
 
 
 ### documentation
 # sample table path
 sample_table_path <- list.files(documentation_path, ".*sampleTable.csv", full.names = T)
-  
+
+# reads stats path
+reads_stats_path <- list.files(mapped_path, ".*\\.stats_and_tracks\\.csv", full.names = T)
+
 
 ### genome
 # genome path
@@ -73,12 +78,14 @@ genome_dir <- "/common/DB/genome_reference/mouse/mm10.GRCm38.GCA_000001635.2"
 genes_info_path <- list.files(path = genome_dir, pattern = str_c("ensembl.", ensembl_version, ".*UCSCseqnames.geneInfo.csv$"), full.names = T)
 
 # reduced exons path
-exons_path <- list.files(path = genome_dir, pattern = str_c("ensembl.", ensembl_version, ".*UCSCseqnames.reducedExons.RDS$"), full.names = T)
-
+exons_path <- file.path(se_main_path, "ensembl.93.GRCm38.p6.20180919.UCSCseqnames.short_Sirena1.reducedExons.RDS")
 
 ######################################################## READ DATA
 # read sample table
 sample_table <- data.table::fread(sample_table_path)
+
+# read stats and tracks table
+reads_stats <- data.table::fread(reads_stats_path)
 
 # summarizedExperiment 
 se <- readRDS(se_path)
@@ -90,6 +97,9 @@ genes_info <- readr::read_csv(genes_info_path)
 exons_gr <- readRDS(file = exons_path)
 
 ######################################################## MAIN CODE
+# join sample table with stats and tracks
+sample_table[reads_stats, on = "sample_id", `:=`(library_size = genome.mapped_minus_rDNA)]
+
 # get total length of all exons for each gene
 exons_table <-
   width(exons_gr) %>%
@@ -105,12 +115,12 @@ fpkm_long_tb <-
        id.vars = c("gene_id"),
        variable.name = "sample_id", 
        value.name = "counts") %>% 
-  .[, sample_id := str_remove(sample_id, ".genome.Aligned.sortedByCoord.out.bam|.total.bam")] %>% 
+  .[, sample_id := str_remove(sample_id, ".genome.Aligned.sortedByCoord.out.bam|.total.bam|.bam")] %>% 
   .[sample_table[, -"bam_path"], on = "sample_id", `:=`(fpm = (counts / round(library_size / 1E6, 6)), 
                                                         stage = i.stage)] %>% 
   .[exons_table, on = "gene_id", fpkm := (fpm / round(width / 1E3, 3))] %>% 
   .[]
-  
+
 # wide and save
 fpkm_wide_tb <- 
   fpkm_long_tb %>% 
@@ -130,7 +140,6 @@ fpkm_stats <-
   .[, list(avg_fpkm = mean(fpkm), 
            SD = sd(fpkm), 
            SE = standard.error(fpkm)), 
-    by = .(gene_id, stage)] %>% 
+    by = .(gene_id, stage)] %T>% 
   write_csv(., file.path(outpath, basename(se_path) %>% str_replace(., ".se.RDS", ".FPKM_statistics.csv")))
-
 
---
+++ papers/Sirena1_2019/tab_S4/02b.FPKM_tables.script.R
@@ -0,0 +1,153 @@
+#!/common/WORK/fhorvat/programi/R/R/bin/Rscript
+### INFO:
+### DATE: Thu Apr 25 16:16:56 2019
+### AUTHOR: Filip Horvat
+rm(list = ls()); gc()
+
+######################################################## WORKING DIRECTORY
+setwd(".")
+
+######################################################## LIBRARIES
+library(dplyr)
+library(readr)
+library(magrittr)
+library(stringr)
+library(tibble)
+library(tidyr)
+library(ggplot2)
+library(data.table)
+library(purrr)
+
+library(GenomicAlignments)
+library(GenomicFeatures)
+library(GenomicRanges)
+
+######################################################## SOURCE FILES
+
+######################################################## FUNCTIONS
+# calculate standard error of the mean FPKM value
+standard.error <- function(x) {
+  sqrt(var(x) / length(x))
+}
+
+######################################################## PATH VARIABLES
+### in and out
+# set inpath
+inpath <- getwd()
+
+# set outpath
+outpath <- getwd()
+
+# get arguments from command line, transform to named vector
+args <-
+  commandArgs(trailingOnly = TRUE) %>%
+  parseCommandLineArguments(.)
+
+# arguments from command line
+experiment <- args$experiment
+single_end <- as.logical(args$single_end)
+threads <- as.numeric(args$threads)
+mapped_path <- args$mapped_path
+documentation_path <- args$documentation_path
+features_coordinates <- args$features_coordinates
+features_name <- args$features_name
+genes_info_path <- args$genes_info_path
+grouping_variables <- args$grouping_variables
+counts_path <- args$counts_path
+original_tb_path <- "../2019 Ganesh Table S4 Elob pseudogenes.xlsx"
+
+# sample table path
+sample_table_path <- list.files(documentation_path, ".*sampleTable\\.csv", full.names = T)
+
+# reads stats path
+reads_stats_path <- list.files(mapped_path, ".*\\.stats_and_tracks\\.csv", full.names = T)
+
+######################################################## READ DATA
+# read counts from featureCounts
+counts_tb <-
+  readr::read_delim(counts_path, delim = "\t", comment = "#") %>%
+  set_colnames(., basename(colnames(.)))
+
+# read sample table
+sample_table <- data.table::fread(sample_table_path)
+
+# read stats and tracks table
+reads_stats <- data.table::fread(reads_stats_path)
+
+# read genes info
+genes_info <- data.table::fread(genes_info_path)
+
+# read original table
+original_tb <- openxlsx::read.xlsx(original_tb_path)
+
+######################################################## MAIN CODE
+### prepare tables
+# get feature coordinates
+features_tb <- 
+  counts_tb %>% 
+  dplyr::select(gene_id = Geneid, seqnames = Chr, start = Start, end = End, width = Length) %>% 
+  as.data.table(.)
+
+# counts table
+counts_tb %<>%
+  dplyr::select(-c(Chr:Length)) %>% 
+  dplyr::rename(gene_id = Geneid) %>% 
+  as.data.table(.)
+
+# join sample table with stats and tracks
+sample_table[reads_stats, on = "sample_id", `:=`(library_size = genome.mapped_minus_rDNA)]
+
+
+### calculate FPKMs
+# long FPKM/FPM/count table
+fpkm_long_tb <-
+  counts_tb %>% 
+  melt(.,
+       id.vars = c("gene_id"),
+       variable.name = "sample_id",
+       value.name = "counts") %>%
+  .[, sample_id := str_remove_all(sample_id, "\\.21to23nt|\\.24to31nt|\\.genome\\.Aligned\\.sortedByCoord\\.out\\.bam|\\.total\\.bam|\\.bam")] %>%
+  .[sample_table, on = "sample_id"] %>%
+  .[features_tb, on = "gene_id", `:=`(width = width)] %>%
+  .[, fpm := (counts / round(library_size / 1E6, 6))] %>%
+  .[, fpkm := (fpm / round(width / 1E3, 3))] %>%
+  .[, c("gene_id", "sample_id", "counts", "fpm", "fpkm", grouping_variables), with = F] %>% 
+  .[!is.na(gene_id), ]
+
+# save long FPKM table
+fpkm_long_tb %>%
+  .[, -grouping_variables, with = F] %T>%
+  readr::write_csv(., file.path(outpath, str_c(features_name, ".FPKM_long.csv")))
+
+# save wide FPKM table
+fpkm_wide_tb <-
+  fpkm_long_tb %>%
+  dcast(., gene_id ~ sample_id, value.var = "fpkm") %>%
+  .[] %T>%
+  write_csv(., file.path(outpath, str_c(features_name, ".FPKM.csv")))
+
+# average FPKM across stages and/or genotype
+fpkm_mean_tb <-
+  fpkm_long_tb %>%
+  .[, .(fpkm_mean = round(mean(fpkm), 3)), by = c("gene_id", grouping_variables)] %>%
+  .[, grouped_variables := do.call(str_c, c(.SD, sep = "_")), .SDcols = grouping_variables] %>%
+  dcast(., gene_id ~ grouped_variables, value.var = "fpkm_mean") %>%
+  .[] %T>%
+  write_csv(., file.path(outpath, str_c(features_name, ".FPKM_mean.csv")))
+
+# long FPKM with mean, SD and standard error for each gene
+fpkm_stats <-
+  fpkm_long_tb %>%
+  .[, list(avg_fpkm = mean(fpkm),
+           SD = sd(fpkm),
+           SE = standard.error(fpkm)),
+    by = c("gene_id", grouping_variables)] %>%
+  .[] %T>%
+  write_csv(., file.path(outpath, str_c(features_name, ".FPKM_stats.csv")))
+
+# join with original table
+original_tb %>% 
+  dplyr::left_join(., fpkm_mean_tb %>% dplyr::rename(Fugaku_GV.WE_FPKM = GV.WE), by = c("ID" = "gene_id")) %T>%
+  openxlsx::write.xlsx(., file.path(outpath, original_tb_path %>% basename(.) %>% str_replace(., "\\.xlsx", ".with_Fugaku_FPKM.xlsx")))
+
+
