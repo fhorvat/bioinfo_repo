b198eb88f516f4cb4180a8f5993c1d6e19f0e7e8
fhorvat <fihorvat@yahoo.com>
2020-06-13 06:47:51 +0200

    added ifelse statment to countFeatures

--- 03.backup_scripts_with_dir_tree.sh
+++ 03.backup_scripts_with_dir_tree.sh
@@ -1,2 +1,9 @@
+# remove existing backup
+find . -name "scripts_in_dirs.tar.gz" -exec rm {} \;
+
+# change dir and find files, add to tar
 cd /common/WORK/fhorvat/Projekti/Svoboda
-find . \( -name "*sh" -or -name "0*txt" \) -print0 | xargs -0 tar -zcf ./scripts/scripts_in_dirs.tar.gz
+find . \( -name "*sh" -or -name "0*txt" \) -exec tar -rvf ./scripts/scripts_in_dirs.tar {} \;
+
+# gzip tar
+gzip ./scripts/scripts_in_dirs.tar
--- differential_expression/RNAseq/01.featureCounts.run.pbs.sh
+++ differential_expression/RNAseq/01.featureCounts.run.pbs.sh
@@ -19,14 +19,16 @@ THREADS=12
 IN_SEQ=($(find $MAPPED_PATH -maxdepth 1 -name "*.bam"))
 IN_SEQ=`printf "%s " ${IN_SEQ[@]}`
 
-# script parameters
-SCRIPT_PARAMS="-T $THREADS \
--F GTF \
--M \
--O \
---fraction"
-# -p 
+# script parameters - single or paired end
+if [ $SINGLE_END == TRUE ]
+then
+   SCRIPT_PARAMS="-T $THREADS -F GTF -M -O --fraction"
+else
+   SCRIPT_PARAMS="-T $THREADS -F GTF -M -O --fraction -p"
+fi
 
 # ----------------Commands------------------- #
 # run script
+echo "Running feature counts with following parameters:" $SCRIPT_PARAMS
+echo "Single end reads are" $SINGLE_END
 featureCounts -a $FEATURES_COORDINATES -o ${FEATURES_NAME}.counts.txt $IN_SEQ $SCRIPT_PARAMS
